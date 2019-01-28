945f0bdc8ed31c9659534a1adad43dcc0d79f910
parkkuri <42799354+parkkuri@users.noreply.github.com>
2019-01-28 09:38:49 +0900

    가위바위보 분류

---
+++ 3_classifier.py
@@ -0,0 +1,44 @@
+# <h3>3. 가위바위보 데이터에 대해 Classifier를 적합해주세요.
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+# Initialising the CNN
+classifier = Sequential()
+# Step 1 - Convolution
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+# Adding a second convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+# Step 3 - Flattening
+classifier.add(Flatten())
+# Step 4 - Full connection
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 3, activation = 'softmax'))
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+from keras.preprocessing.image import ImageDataGenerator
+# 데이터 생성하기
+train_datagen = ImageDataGenerator(rescale = 1./255,
+shear_range = 0.2,
+zoom_range = 0.2,
+horizontal_flip = False)
+test_datagen = ImageDataGenerator(rescale = 1./255)
+training_set = train_datagen.flow_from_directory('C:/Users/kebee/Desktop/toobig/toobig_8th/Dataset/Dataset/model/train',
+target_size = (64, 64),
+batch_size = 32)
+test_set = test_datagen.flow_from_directory('C:/Users/kebee/Desktop/toobig/toobig_8th/Dataset/Dataset/model/test',
+target_size = (64, 64),
+batch_size = 32)
+#모델 학습과정 설정하기
+classifier.fit_generator(training_set,
+steps_per_epoch = 300,
+epochs = 25,
+validation_data = test_set,
+validation_steps = 2000)
+#정확도
+scores = classifier.evaluate_generator(test_set, steps=5)
+print("%s: %.2f%%" %(classifier.metrics_names[1], scores[1]*100))
